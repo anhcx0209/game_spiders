886c2470a08e128957459ab4344e41f85b31908c
anhcx0209 <anhcx0209@gmail.com>
2016-01-14 01:47:16 +0300

    Add components for main windows - class Spiders
    Change class Spiders to extends JPanel and add all Jcomponents for
    gaming.

--- Spiders/src/spiders/Spiders.java
+++ Spiders/src/spiders/Spiders.java
@@ -5,17 +5,134 @@
  */
 package spiders;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
 /**
- *
+ * Main class
  * @author anhcx
  */
-public class Spiders {
+public class Spiders extends JFrame {
+    
+    // ------------ COMPONENTS ---------------------
+    private JPanel _controlPanel = new JPanel(new FlowLayout());
+    private JLabel _bestResultInfo = new JLabel();
+    private JLabel _stepInfo = new JLabel();
+    private JPanel _content = new JPanel();
+    
+    // ----------- MENU BAR ------------------------
+    private JMenuBar _menu = null;
+    
+    private void createMenu() {
+        _menu = new JMenuBar();
+        JMenu fileMenu = new JMenu("Game");
+        String menuItems[] = new String []{"New", "High Score", "Exit"};
+        
+        for (int i = 0; i < menuItems.length; i++) {
+            JMenuItem item = new JMenuItem(menuItems[i]);
+            item.setActionCommand(menuItems[i].toLowerCase());
+            item.addActionListener(new FileMenuListener());
+            fileMenu.add(item);
+        }
+        fileMenu.insertSeparator(1);
 
+        _menu.add(fileMenu);
+    }
+
+    /**
+     * Menu listener
+     */
+    private static class FileMenuListener implements ActionListener {
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            
+            if ("exit".equals(command)) {
+                System.exit(0);
+            }
+            
+            if ("new".equals(command)) {
+                Object[] possibleValues = { "Easy", "Medium", "Hard" };
+                Object selectedValue = JOptionPane.showInputDialog(null,
+                "Choose one level", "Game Level",  JOptionPane.INFORMATION_MESSAGE, null,
+                possibleValues, possibleValues[0]);
+            }
+            
+            if ("high score".equals(command)) {
+                // read and show high score
+            }
+        }
+    }
+    
+    // ------------- GAME INFO PANEL ----------------------
+    private Box createInfoPanel() {
+        
+        Box box = Box.createHorizontalBox();
+        
+        box.add(Box.createHorizontalStrut(10));
+        box.add(new JLabel("Step :"));
+        _stepInfo.setText("?");
+        box.add(Box.createHorizontalStrut(10));
+        box.add(_stepInfo);
+        
+        box.add(Box.createHorizontalStrut(20));
+
+        box.add(new JLabel("Best result :"));
+        box.add(Box.createHorizontalStrut(10));
+        _bestResultInfo.setText("?");
+        box.add(Box.createHorizontalStrut(10));
+        box.add(_bestResultInfo);
+
+        box.add(Box.createHorizontalStrut(10));
+        
+        return box;
+    }
+    
+    /**
+     * Create a new game window
+     */
+    public Spiders() {
+        // add control panel and game panel to content
+        _controlPanel.add(createInfoPanel());
+        _content.setLayout(new BorderLayout());
+        _content.add(_controlPanel, BorderLayout.NORTH);
+        setContentPane(_content);
+        
+        // add menu bar
+        createMenu();
+        setJMenuBar(_menu);
+        
+        setTitle("Пауки");
+        
+        // pre setup for window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Spiders();
+            }
+        });
     }
     
 }
