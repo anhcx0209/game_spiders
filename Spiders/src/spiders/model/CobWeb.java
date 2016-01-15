5f32169fa107cf27f6894ad94dbcf9a605e0b872
anhcx0209 <anhcx0209@gmail.com>
2016-01-15 22:55:39 +0300

    add constructor - do nothing

--- Spiders/src/spiders/model/CobWeb.java
+++ Spiders/src/spiders/model/CobWeb.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spiders.model;
 
+import java.util.ArrayList;
+import spiders.model.CobWebObject.TypeObject;
+
 /**
- *
+ * Cobweb - where spider and bug move
+ * size of cobweb = n x n
  * @author anhcx
  */
 public class CobWeb {
     
+    // ------------------ size --------------------
+    int _size;
+    
+    CobWeb(int s) {
+        _size = s;
+    }
+    
+    public int size() {
+        return _size;
+    }
+    
+    public void setSize(int s) {
+        _size = s;
+    }
+    
+    public CobWeb() {
+        
+    }
+    
+    // ------------------- objects on cobweb --------
+    ArrayList<CobWebObject> _objects = new ArrayList<>();
+    
+    public void addObject(CobWebObject obj) {
+        _objects.add(obj);
+    }
+    
+    public void removeObject(CobWebObject obj) {
+        if (_objects.contains(obj))
+            _objects.remove(obj);
+    }
+    
+    public ArrayList<CobWebObject> objects() {
+        return _objects;
+    }
+    
+    public ArrayList<CobWebObject> objects(TypeObject type) {
+        ArrayList<CobWebObject> ret = new ArrayList<>();
+        for (CobWebObject e : _objects) {
+            if (e.type() == type) {
+                ret.add(e);
+            }
+        }
+        return ret;
+    }
 }
