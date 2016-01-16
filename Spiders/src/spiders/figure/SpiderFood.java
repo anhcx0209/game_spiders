372587b01f29a9d5d829da59edc6fe687e798656
anhcx0209 <anhcx0209@gmail.com>
2016-01-16 16:02:42 +0300

    create spiderfood

---
+++ Spiders/src/spiders/figure/SpiderFood.java
@@ -0,0 +1,59 @@
+package spiders.figure;
+
+import java.util.Random;
+import spiders.model.CobWeb;
+import spiders.model.CobWebObject;
+import spiders.navigations.Position;
+
+/**
+ * Bug, which is food of spider.
+ * They can fall in to cobweb and escape from it.
+ * They can be eaten by spider, spider will convert their value to life.
+ * @author anhcx
+ */
+public class SpiderFood extends CobWebObject {
+
+    /**
+     * Generate a spider's food
+     * @param cw cobweb which food can be fall into.
+     */
+    public SpiderFood(CobWeb cw) {
+        super(cw);
+    }
+    
+    
+    // ------------------ size ----------------------
+    // size of food, it is value life will be increase when spider eat
+    int _size;          
+    
+    public int size() {
+        return _size;
+    }
+    
+    /**
+     * bug fall in to web an be sticked there.
+     * @return action successful or not
+     */
+    public boolean failIntoWeb() {
+        Random rn = new Random();
+        int factor = rn.nextInt(10) + 1;
+        if (factor == 1 || factor == 2 || factor == 3) {
+            Position pos = cobweb().getFreePosition();
+            if (pos != null) {
+                setPosition(pos);
+                return true;
+            } else 
+                return false;
+        }
+        else 
+            return false;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public boolean escape() {
+        return true;
+    }
+}
