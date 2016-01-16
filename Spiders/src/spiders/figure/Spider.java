a45548f8d23fffb29c7bc238788ec884f76afceb
anhcx0209 <anhcx0209@gmail.com>
2016-01-16 11:03:43 +0300

    Base-on Class spider

---
+++ Spiders/src/spiders/figure/Spider.java
@@ -0,0 +1,52 @@
+package spiders.figure;
+
+import spiders.model.CobWeb;
+import spiders.model.CobWebObject;
+import spiders.navigations.Direction;
+import spiders.navigations.Position;
+
+/**
+ * Spider - actor, can eat some bug, move, and be stunned by drop rain for a 
+ * couple of seconds.
+ * @author anhcx
+ */
+public class Spider extends CobWebObject  {
+
+    /**
+     * generate a spider with his cobweb
+     * @param cw - cobweb, which spiders move on
+     */
+    public Spider(CobWeb cw) {
+        super(cw);
+    }
+    
+    // ------------------- life ----------------
+    private int _life;      // store life, when life = 0, spider die
+    
+    public int life() {
+        return _life;
+    }
+            
+    /**
+     * decrease spider's life
+     * @param amount - number of life is decreased
+     */
+    public void decreaseLife(int amount) {
+        _life -= amount;
+    }
+    
+    /**
+     * increase spider's life
+     * @param amount - number of life is added
+     */
+    public void increaseLife(int amount) {
+        _life += amount;
+    }
+    
+    public void move(Direction direct) {
+    }
+    
+    private boolean moveIsPossible(Direction direct) {
+        return false;
+    }
+}
