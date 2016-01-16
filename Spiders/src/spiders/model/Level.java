7dfc01a328ada57b6068445036f08eed40e7d8af
anhcx0209 <anhcx0209@gmail.com>
2016-01-16 22:46:52 +0300

    add constant and spin function

--- Spiders/src/spiders/model/Level.java
+++ Spiders/src/spiders/model/Level.java
@@ -6,6 +6,7 @@
 package spiders.model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Game's level
@@ -15,10 +16,15 @@ import java.util.ArrayList;
  * @author anhcx
  */
 public class Level {
+    // ----------------- CONSTANT --------------------
+    private static final int N_FF_HARD = 10;
+    private static final int N_FF_MEDIUM = 15;
+    private static final int N_FF_EASY = 20;
     
     private int _baseSize;
     private int _numberOfComputer;
     private int _numberBug;
+    private int[] _factorGenFood; 
     
     /**
      * @return number of bug must be generate per time.
@@ -41,6 +47,16 @@ public class Level {
         return _baseSize;
     }
     
+    public boolean spin() {
+        Random rand = new Random();
+        int x = rand.nextInt(100);
+        for (int i = 0; i < _factorGenFood.length; i++)
+            if (_factorGenFood[i] == x)
+                return true;
+        
+        return false;
+    }
+    
     /**
      * create a hard level
      * base size = 15
@@ -51,6 +67,8 @@ public class Level {
         l._baseSize = 12;
         l._numberOfComputer = 3;
         l._numberBug = 20;
+        l.makeFactor(N_FF_HARD);
+        
         return l;
     }
     
@@ -64,6 +82,9 @@ public class Level {
         l._baseSize = 6;
         l._numberOfComputer = 2;
         l._numberBug = 6;
+        l.makeFactor(N_FF_EASY);
+        
+        int x = 100;
         return l;
     }
     
@@ -77,7 +98,26 @@ public class Level {
         l._baseSize = 10;
         l._numberOfComputer = 2;
         l._numberBug = 8;
+        l.makeFactor(N_FF_MEDIUM);
         return l;
     }
     
+    
+    private void makeFactor(int n) {
+        _factorGenFood = new int[n]; // 10 % gen food
+        
+        boolean[] flag = new boolean[100];
+        Random rn = new Random();
+        for (int i = 0; i < n; i++) {
+            boolean ok = true;
+            while (ok) {
+                int k = rn.nextInt(100);
+                if (!flag[k]) {
+                    flag[k] = true;
+                    _factorGenFood[i] = k;
+                    ok = false;
+                }
+            }
+        }
+    }
 }
