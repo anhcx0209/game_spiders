a8c24cb5bff5f0db587b8e6b13962ef50b6a5f48
anhcx0209 <anhcx0209@gmail.com>
2016-01-17 16:32:28 +0300

    Reflection all method from Stone to Risk
    _stunTime
    _spider
    spiderMoing

--- Spiders/src/spiders/figure/Stone.java
+++ Spiders/src/spiders/figure/Stone.java
@@ -8,7 +8,7 @@ import spiders.model.CobWeb;
  * Spider move to stone will be turn back and stun for 2 system step.
  * @author anhcx
  */
-public class Stone extends Risk implements SpiderActionListener {
+public class Stone extends Risk {
 
     public Stone(CobWeb cw) {
         super(cw);
@@ -18,39 +18,4 @@ public class Stone extends Risk implements SpiderActionListener {
         _spider = null;
     }
     
-    public boolean busy() {
-        return _spider != null;
-    }
-    
-    private Spider _spider;
-
-    /**
-     * Spider s move to stone.
-     * @param s who is moving to.
-     */
-    @Override
-    public void spiderMoving(Spider s) {
-        // if stone is busy to detaine someone
-        if (busy()) {
-            // if spider is "someone"
-            if (s.equals(_spider)) {
-                _stunTime--;
-                System.err.println(s.name() + "try to move but not....");
-                System.err.println("remain time: " + _stunTime);
-
-                if (_stunTime == 0) {
-                    _spider = null;
-                    s.unStun();
-                    _stunTime = 2;
-                }
-            }
-        } else {
-            // if stone is free, 
-            if (s.position().equals(position()) && !s.isStuned()) {
-                System.err.println(s.name() + " is stuned at " + position().toString());
-                _spider = s;
-                s.stun();
-            }
-        }
-    }
 }
