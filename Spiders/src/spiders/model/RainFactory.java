975ce442f20c4ab20424a250641888a94e6ce3ba
anhcx0209 <anhcx0209@gmail.com>
2016-01-17 15:57:51 +0300

    rain factory - create rain at starting of game

--- Spiders/src/spiders/model/RainFactory.java
+++ Spiders/src/spiders/model/RainFactory.java
@@ -1,9 +1,28 @@
 package spiders.model;
 
+import java.util.ArrayList;
+import spiders.figure.Rain;
+import spiders.navigations.Position;
+
 /**
  * Factory to generate drop rain.
  * @author anhcx
  */
 public class RainFactory {
+    private GameModel _gameModel;
     
+    public RainFactory(GameModel gm) {
+        _gameModel = gm;
+    }
+    
+    public ArrayList<Rain> createRains() {
+        ArrayList<Rain> ret = new ArrayList<>();
+        for (int i = 1; i <= _gameModel.field().size(); i++) {
+            Rain r = new Rain(_gameModel.field());
+            r.setPosition(new Position(1, i));
+            ret.add(r);
+        }
+        
+        return ret;
+    }
 }
