4dd886dabf95e90d74c549567e4b04222a349bd2
anhcx0209 <anhcx0209@gmail.com>
2016-01-17 13:50:39 +0300

    Class stone
    when spider moving, do action in function spiderMoving

--- Spiders/src/spiders/figure/Stone.java
+++ Spiders/src/spiders/figure/Stone.java
@@ -1,5 +1,6 @@
 package spiders.figure;
 
+import spiders.events.SpiderActionListener;
 import spiders.model.CobWeb;
 
 /**
@@ -7,13 +8,49 @@ import spiders.model.CobWeb;
  * Spider move to stone will be turn back and stun for 2 system step.
  * @author anhcx
  */
-public class Stone extends Risk {
+public class Stone extends Risk implements SpiderActionListener {
 
     public Stone(CobWeb cw) {
         super(cw);
         _type = TypeObject.STONE;
         _stunTime = 2;
         _position = cobweb().getFreePosition();
+        _spider = null;
     }
     
+    public boolean busy() {
+        return _spider != null;
+    }
+    
+    private Spider _spider;
+
+    /**
+     * Spider s move to stone.
+     * @param s who is moving to.
+     */
+    @Override
+    public void spiderMoving(Spider s) {
+        // if stone is busy to detaine someone
+        if (busy()) {
+            // if spider is "someone"
+            if (s.equals(_spider)) {
+                _stunTime--;
+                System.err.println(s.name() + "try to move but not....");
+                System.err.println("remain time: " + _stunTime);
+
+                if (_stunTime == 0) {
+                    _spider = null;
+                    s.unStun();
+                    _stunTime = 2;
+                }
+            }
+        } else {
+            // if stone is free, 
+            if (s.position().equals(position()) && !s.isStuned()) {
+                System.err.println(s.name() + " is stuned at " + position().toString());
+                _spider = s;
+                s.stun();
+            }
+        }
+    }
 }
