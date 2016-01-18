38e815fac98d8dfb8673114021dc41bfee5b7ef4
anhcx0209 <anhcx0209@gmail.com>
2016-01-18 13:03:47 +0300

    change food factory to generate bug with diff

--- Spiders/src/spiders/model/FoodFactory.java
+++ Spiders/src/spiders/model/FoodFactory.java
@@ -5,7 +5,12 @@
  */
 package spiders.model;
 import java.util.ArrayList;
+import java.util.Random;
+import spiders.figure.Fly;
+import spiders.figure.Grasshopper;
+import spiders.figure.Mosquito;
 import spiders.figure.SpiderFood;
+import spiders.figure.Wasp;
 
 /**
  * Factory to generate food.
@@ -24,15 +29,33 @@ public class FoodFactory {
     
     /**
      * Generate more food.
+     * @param number - number food want to create
      */
     public ArrayList<SpiderFood> createFood(int number) {
         ArrayList<SpiderFood> ret = new ArrayList<>();
         for (int i = 0; i < number; i++) {
-            SpiderFood sf = new SpiderFood(_gameModel.field());
-            sf.setName("Bug" + i);
-            ret.add(sf);
+            Random rnd = new Random();
+            
+            int f = rnd.nextInt(15) + 1;
+            
+            ret.add(getFood(f));
         }
         return ret;
+    }
+    
+    protected SpiderFood getFood(int n) {
+        if (n >= 1 && n <= 10) {
+            Random random = new Random();
+            if (random.nextBoolean())
+                return new Fly(_gameModel.field());
+            else 
+                return new Mosquito(_gameModel.field());
+        }
+        
+        if (n >= 11 && n <= 13)
+            return new Wasp(_gameModel.field());
+        
+        return new Grasshopper(_gameModel.field());
     }
     
 }
