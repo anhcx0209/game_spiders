40686142d6d30cb3da9575512986a38380c1c8e5
anhcx0209 <anhcx0209@gmail.com>
2016-01-19 02:26:11 +0300

    Change father's name
    + Change name variable to more readable
    + Add javadoc
    + Confuse about function turnBack()

--- Spiders/src/spiders/figure/Rain.java
+++ Spiders/src/spiders/figure/Rain.java
@@ -1,46 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spiders.figure;
 
-import spiders.events.SpiderActionListener;
+import java.util.Random;
 import spiders.model.CobWeb;
 import spiders.navigations.Direction;
 import spiders.navigations.Position;
 
 /**
- * Rain is risk, can move down and can stun food and spider.
+ * Rain is holder, can move down and can capture food and spider.
  * Rain always start moving from row 1.
  * When go to last row, it will disappear.
  * @author anhcx
  */
-public class Rain extends Risk {
+public class Rain extends Holder {
 
+    /**
+     * Create a drop of rain.
+     * @param cw cobweb where rain move on.
+     */
     public Rain(CobWeb cw) {
         super(cw);
         _type = TypeObject.RAIN;
-        _stunTime = 3;
+        _timeHoldSpider = 3;
         _spider = null;
+        _timeHoldFood = 2;
         _food = null;
-        _timeStunFood = 2;
     }
 
-    protected SpiderFood _food;
-    private int _timeStunFood;
+    // -------------------- HOLDING FOOD -----------------------
+    private SpiderFood _food;
+    private int _timeHoldFood;
     
-    protected boolean busyWithFood() {
+    private boolean isHoldingFood() {
         return (_food != null);
     }
-     
+    
+    private void releaseFood() {
+        _food.unStun();
+        _food = null;
+    }
+    
+    private void captureFood(SpiderFood f) {
+        _food = f;
+        _food.stun();
+    }
+    
+    public void turnBack() {
+        Random rand = new Random();
+        int j = 1 + rand.nextInt(cobweb().size());
+        setPosition(new Position(1, j));
+        
+        if (cobweb().have(TypeObject.PLAYER, _position) || cobweb().have(TypeObject.COMPUTER, _position)) {
+            _spider = cobweb().getSpider(_position);
+        }
+        
+        if (cobweb().have(TypeObject.FOOD, _position)) {
+            _food = cobweb().getFood(_position);
+        }
+    }
     
     /**
-     * Rain move down.
+     * Rain move down and capture every spider or food they can.
+     * If rain is holding some food, it will decrease time and let move on 
+     * when out of time.
      */
     public void moveDown() {
         
-        if (!busy() && !busyWithFood()) {
+        if (!isHoldingSpider()&& !isHoldingFood()) {
             Direction dir = Direction.south();
             Position newPos = position().next(dir);
             if (newPos.isValid())
@@ -48,29 +73,25 @@ public class Rain extends Risk {
             
             // if rain move to pos of player or computer - spider on web.
             if (cobweb().have(TypeObject.PLAYER, newPos) || cobweb().have(TypeObject.COMPUTER, newPos)) {
-                // stun it;
-                _spider = cobweb().getSpider(newPos);
-                _spider.stun();
+                captureSpider(cobweb().getSpider(newPos));
                 System.err.println(_spider.name() + " is stuned at " + position().toString());
             }
             
             // if rain move to pos of food.
             if (cobweb().have(TypeObject.FOOD, newPos)) {
                 // stun it;
-                _food = cobweb().getFood(newPos);
-                _food.stun();
+                captureFood(cobweb().getFood(newPos));
                 System.err.println(_food.name() + " is stuned at " + position().toString());
             }
         } else {
-            if (busyWithFood()) {
-                _timeStunFood--;
-                System.err.println(_food.name() + "try to move but not....");
-                System.err.println("remain time: " + _timeStunFood);
+            if (isHoldingFood()) {
+                _timeHoldFood--;
+                System.err.println(_food.toString() + "try to move but not....");
+                System.err.println("remain time: " + _timeHoldFood);
 
-                if (_timeStunFood == 0) {
-                    _food.unStun();
-                    _food = null;
-                    _timeStunFood = 2;
+                if (_timeHoldFood == 0) {
+                    releaseFood();
+                    _timeHoldFood = 2;
                 }
             }
         }
