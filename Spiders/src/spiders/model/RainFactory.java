2efd6d0768fdd4468cde0a3a66da11b00e89d6a7
anhcx0209 <anhcx0209@gmail.com>
2016-01-19 02:28:47 +0300

    Adding java doc
    + Add java doc.
    + use function turnBack() to positioning rain. <confuse>

--- Spiders/src/spiders/model/RainFactory.java
+++ Spiders/src/spiders/model/RainFactory.java
@@ -1,25 +1,50 @@
 package spiders.model;
 
 import java.util.ArrayList;
+import java.util.Random;
+import spiders.figure.Computer;
 import spiders.figure.Rain;
-import spiders.navigations.Position;
 
 /**
- * Factory to generate drop rain.
+ * Factory to generate rain.
  * @author anhcx
  */
 public class RainFactory {
     private GameModel _gameModel;
     
+    /**
+     * Constructor to create rain factory.
+     * @param gm game model which factory will base on.
+     */
     public RainFactory(GameModel gm) {
         _gameModel = gm;
     }
     
-    public ArrayList<Rain> createRains() {
+    /**
+     * Create n drop rain on the first line of cobweb.
+     * Position is randomly selected.
+     * @param n number of rain will be created.
+     * @return list of rains.
+     */
+    public ArrayList<Rain> createRains(int n) {
         ArrayList<Rain> ret = new ArrayList<>();
-        for (int i = 1; i <= _gameModel.field().size(); i++) {
+        
+        Random rand = new Random();
+        
+        for (int i = 1; i <= n; i++) {
+            // create rain
             Rain r = new Rain(_gameModel.field());
-            r.setPosition(new Position(1, i));
+            
+            r.turnBack();
+            
+            // add spider listener
+            _gameModel.player().addSAL(r);
+            for (Computer com : _gameModel.computers()) {
+                com.addSAL(r);
+            }
+            
+            _gameModel.field().addObject(r);
+            
             ret.add(r);
         }
         
