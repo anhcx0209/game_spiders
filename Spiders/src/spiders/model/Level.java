3799e91d443f4492575b942a658bb9d9d00ed595
anhcx0209 <anhcx0209@gmail.com>
2016-01-18 22:41:58 +0300

    Add javadoc and adjust factors each level
    + Rename some variable
    + Adjust some factor to balance levels
    + Add comment to generate javadoc

--- Spiders/src/spiders/model/Level.java
+++ Spiders/src/spiders/model/Level.java
@@ -1,60 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package spiders.model;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Game's level
- * Hard - 
- * Easy - 
- * Medium - 
+ * Difficulty level of the game, its depends on some factor: size of field, 
+ * number of computers will play with player, how frequently generate food 
+ * after each step, number of stones, number of rains will threat spiders.
+ * There are 3 different level: HARD, MEDIUM and EASY. 
  * @author anhcx
  */
 public class Level {
-    // ----------------- CONSTANT --------------------
-    private static final int N_FF_HARD = 10;
-    private static final int N_FF_MEDIUM = 15;
-    private static final int N_FF_EASY = 20;
     
-    private int _baseSize;
-    private int _numberOfComputer;
-    private int _numberBug;
-    private int _numberStone;
-    private int[] _factorGenFood; 
+    // ----------------- FREQUENCY GENERTATE FOODS---------------------
+    private static final int N_FF_HARD = 10;    // FOR HARD = 10%
+    private static final int N_FF_MEDIUM = 15;  // FOR MEDIUM = 15%
+    private static final int N_FF_EASY = 20;    // FOR EASY = 20%
+    
+    // ----------------- FACTOR DERTEMINE DIFFICULTY OF GAME ---------
+    private int _baseSize;              // size of field.
+    private int _numberOfComputers;     // number of computers will play.
+    private int _numberOfFoods;         // number of foods will appear after each step.
+    private int _numberOfStones;        // number of stones in the field.
+    private int _numberOfRains;         // number of rains in the field.
+    private int[] _factorGenFood;       // frequency generate food after each step.
     
     /**
-     * @return number of stone.
+     * This is getter, return number of rains on the field.
+     * @return number of rains on the field.
      */
-    public int numberStone() {
-        return _numberStone;
+    public int numberOfRain() {
+        return _numberOfRains;
     }
     
     /**
+     * This is getter, return number of stones on the field.
+     * @return number of stones.
+     */
+    public int numberOfStone() {
+        return _numberOfStones;
+    }
+    
+    /**
+     * This is getter, return number of foods will be generated.
      * @return number of bug must be generate per time.
      */
-    public int numberBug() {
-        return _numberBug;
+    public int numberOfFoods() {
+        return _numberOfFoods;
     }
     
     /**
-     * @return number of computer in start of game
+     * This is getter, return number of spiders will be controlled by computers.
+     * @return number of computer in start of game.
      */
-    public int numberCom() {
-        return _numberOfComputer;
+    public int numberOfComputers() {
+        return _numberOfComputers;
     }
     
     /**
-     * @return size of game field
+     * This is getter, return size of field will be generated.
+     * @return size of game field.
      */
     public int baseSize() {
         return _baseSize;
     }
     
+    /**
+     * This method will determine generating more food after each step or not.
+     * @return boolean generate or not.
+     */
     public boolean spin() {
         Random rand = new Random();
         int x = rand.nextInt(100);
@@ -65,53 +78,56 @@ public class Level {
         return false;
     }
     
+    // --------------------- STATIC FUNCTIONS ----------------------------
+    
     /**
-     * create a hard level
-     * base size = 15
-     * @return
+     * Create a level with difficulty - hard.
+     * @return hard level.
      */
     public static Level hard() {
         Level l = new Level();
-        l._baseSize = 12;
-        l._numberOfComputer = 3;
-        l._numberBug = 20;
+        l._baseSize = 14;
+        l._numberOfComputers = 4;
+        l._numberOfFoods = 20;
+        l._numberOfRains = 10;
+        l._numberOfStones = 7;
         l.makeFactor(N_FF_HARD);
-        
         return l;
     }
     
     /**
-     * create easy level
-     * base = 6
-     * @return
-     */
-    public static Level easy() {
-        Level l = new Level();
-        l._baseSize = 9;
-        l._numberOfComputer = 2;
-        l._numberBug = 6;
-        l.makeFactor(N_FF_EASY);
-        l._numberStone = 3;
-        return l;
-    }
-    
-    /**
-     * create a medium level
-     * base size = 10
-     * @return
+     * Create a level with difficulty - medium.
+     * @return medium level.
      */
     public static Level medium() {
         Level l = new Level();
-        l._baseSize = 10;
-        l._numberOfComputer = 2;
-        l._numberBug = 8;
+        l._baseSize = 12;
+        l._numberOfComputers = 3;
+        l._numberOfFoods = 10;
+        l._numberOfRains = 7;
+        l._numberOfStones = 5;
         l.makeFactor(N_FF_MEDIUM);
         return l;
     }
     
+    /**
+     * Create a level with difficulty - easy.
+     * @return easy level.
+     */
+    public static Level easy() {
+        Level l = new Level();
+        l._baseSize = 9;
+        l._numberOfComputers = 2;
+        l._numberOfFoods = 6;
+        l._numberOfRains = 4;
+        l._numberOfStones = 3;
+        l.makeFactor(N_FF_EASY);
+        return l;
+    }
     
+    // ------------------ MAKE FACTOR ----------------------------
     private void makeFactor(int n) {
-        _factorGenFood = new int[n]; // 10 % gen food
+        _factorGenFood = new int[n]; 
         
         boolean[] flag = new boolean[100];
         Random rn = new Random();
