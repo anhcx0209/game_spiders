47f72eaf3ed8151cefb522e4374071441cfd3403
anhcx0209 <anhcx0209@gmail.com>
2016-01-18 13:02:16 +0300

    create fly class

---
+++ Spiders/src/spiders/figure/Fly.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spiders.figure;
+
+import spiders.model.CobWeb;
+
+/**
+ *
+ * @author anhcx
+ */
+public class Fly extends SpiderFood {
+
+    public Fly(CobWeb cw) {
+        super(cw);
+        _type = TypeObject.FLY;
+        randomSize(SizeFood.SMALL);
+    }
+    
+}
